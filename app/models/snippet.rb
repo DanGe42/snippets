71bb862a62e546faeac40efa79efa396fd55b234
Daniel Ge <dange@seas.upenn.edu>
2012-09-15 04:53:27 -0400

    Add methods in User/Snippet models for up/downvoting

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,11 @@
 class Snippet < ActiveRecord::Base
   attr_accessible :code, :points, :title
+
+  def increment_points (incr = 1)
+    self.update_attributes(:points => self.points + incr)
+  end
+
+  def decrement_points (decr = 1)
+    self.update_attributes(:points => self.points - 1)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,69 @@ class User < ActiveRecord::Base
       user.name = auth["info"]["name"]
    end
   end
+
+  def upvote (snippet)
+    usv = UserSnippetVote.new(:user => self, :snippet => snippet,
+                              :vote => UserSnippetVote::UPVOTE)
+
+    if (usv.save)
+      snippet.increment_points
+      return true
+    else
+      prev_vote = UserSnippetVote.find(
+        ["user_id = :user AND snippet_id => :snippet AND vote = :vote",
+          { :user => self.id, :snippet => snippet.id, :vote => UserSnippetVote::DOWNVOTE }])
+
+      # If the previous vote is a downvote
+      if not prev_vote.nil?
+        prev_vote.delete
+        snippet.increment_points(2)
+        usv.save!
+        return true
+      end
+    end
+
+    false
+  end
+
+  def downvote (snippet)
+    usv = UserSnippetVote.new(:user => self, :snippet => snippet,
+                              :vote => UserSnippetVote::DOWNVOTE)
+
+    if (usv.save)
+      snippet.decrement_points
+      return true
+    else
+      prev_vote = UserSnippetVote.find(
+        ["user_id = :user AND snippet_id => :snippet AND vote = :vote",
+          { :user => self.id, :snippet => snippet.id, :vote => UserSnippetVote::DOWNVOTE }])
+
+      # If the previous vote is a downvote
+      if not prev_vote.nil?
+        prev_vote.delete
+        snippet.decrement_points(2)
+        usv.save!
+        return true
+      end
+    end
+
+    false
+  end
+
+  def remove_vote (snippet)
+    usv = UserSnippetVote.find(["user_id = :user AND snippet_id => :snippet",
+                               { :user => self.id, :snippet => snippet.id }])
+
+    return if usv.nil?
+
+    if usv.vote == UserSnippetVote::UPVOTE
+      snippet.decrement_points
+    else
+      snippet.increment_points
+    end
+  end
+
+  private
+  def change_vote (snippet)
+  end
 end
