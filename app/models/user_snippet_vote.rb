e68c78a2414956eb067c39bae0e6cfc5d6743ebb
Daniel Ge <dange@seas.upenn.edu>
2012-09-15 03:02:59 -0400

    Add validations to UserSnippetVote
    Validate that all three parameters (user, snippet, vote) are present and
    that (user, snippet) pairs are unique.

--- app/models/user_snippet_vote.rb
+++ app/models/user_snippet_vote.rb
@@ -1,5 +1,11 @@
 class UserSnippetVote < ActiveRecord::Base
+  UPVOTE = true
+  DOWNVOTE = false
+
   belongs_to :user
   belongs_to :snippet
-  attr_accessible :vote
+  attr_accessible :user, :snippet, :vote
+
+  validates :user, :snippet, :vote, :presence => true
+  validates :user_id, :uniqueness => { :scope => :snippet_id }
 end
