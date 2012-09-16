9bba1cd6cbc19444dacdd15e847a42720f1e2ada
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2012-09-16 02:02:22 -0400

    filepicker and text select inputs

--- app/assets/javascripts/snippets.js
+++
@@ -1,9 +0,0 @@
-filepicker.getFile('text/*', function(url){
-  var results = $('#getContentsText').text("Loading...");
-  filepicker.getContents(url, function(data){
-	$("#result").setAttribute('p', 'testing testing');  
-//  var r = results.text(data);
-//  $("#result").setAttribute('p',text(data));
- //.setAttribute('p', r);
-  });
-});
