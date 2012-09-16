e145c4b7beb07a763e4bdb6bed8875fe841227d4
Daniel Ge <dange@seas.upenn.edu>
2012-09-16 09:47:33 -0400

    Remove weird whitespace from code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :snippet
   attr_accessible :content, :user, :snippet, :user_id, :snippet_id
-  
+
   validates :user, :snippet, :content, :presence => true
   validates :content, :length => {:minimum => 1, :maximum => 500}
-  
+
   def commenter
     User.find(self.user_id)
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :snippets
   has_many :votes
   has_many :comments
-  
+
   validates :name, :presence => true
   attr_accessible :name, :provider, :uid
  
