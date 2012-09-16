822951c4098ffab1e75312dde972a8bc3f4998ba
Daniel Ge <dange@seas.upenn.edu>
2012-09-15 20:25:55 -0400

    Convert snippets/show to HAML and use better button helper
    Haml is better than ERB
    
    Use better_button_to instead of button_to because the original button_to
    is far too limited.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def better_button_to (button_text, action_path, form_options = {}, button_options = {})
+    form_tag(action_path, form_options) do
+      content_tag(:div, :class => "visible-fields") do
+        tag(:input, {:type => "submit", :value => button_text}.merge(button_options), true)
+      end
+    end
+  end
 end
