928fbb3ece135502e87130dc94fe3b23fbb4b91f
Daniel Ge <dange@seas.upenn.edu>
2012-09-15 20:25:55 -0400

    Convert snippets/show to use better button helper
    Use better_button_to instead of button_to because the original button_to
    is far too limited.
    
    Conflicts:
    	app/helpers/application_helper.rb
    	app/views/snippets/show.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     return time_ago_in_words(time)
   end
   
+  def better_button_to (button_text, action_path, form_options = {}, button_options = {})
+    form_tag(action_path, form_options) do
+      content_tag(:div, :class => "visible-fields") do
+        tag(:input, {:type => "submit", :value => button_text}.merge(button_options), true)
+      end
+    end
+  end
+
 end
