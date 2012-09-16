2802ca38248252baeac8b83f85aa9f7a1eb07326
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2012-09-16 10:44:55 -0400

    heroku test, and styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120916050614) do
+ActiveRecord::Schema.define(:version => 20120916100857) do
 
   create_table "comments", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "snippet_id"
     t.integer  "user_id"
+    t.integer  "snippet_id"
+    t.string   "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   add_index "comments", ["snippet_id"], :name => "index_comments_on_snippet_id"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "snippet_tags", :force => true do |t|
     t.integer  "tag_id"
